e0630364e547cf0760d6bc2b4ffda22b7426f7cd
Nur Fajar <nur.fajar11@ui.ac.id>
2024-12-07 11:32:00 +0700

    fix: handle blank page in mobile screen

--- app/onboarding/create/page.tsx
+++ app/onboarding/create/page.tsx
@@ -6,7 +6,7 @@ import { CreateOrganizationForm } from "./create-organization-form"
 
 export default async function Create() {
   return (
-    <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+    <div className="container relative h-screen flex items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
       <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
         <div className="absolute inset-0 bg-zinc-900" />
         <div className="relative z-20 flex items-center text-lg font-medium">
